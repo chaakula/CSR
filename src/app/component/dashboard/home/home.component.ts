4bc27e9db512ed341a7389e3be1315ba14120abb
s729492 <s729492@dnata.com>
2017-10-10 21:59:54 +0400

    done

--- src/app/component/dashboard/home/home.component.ts
+++ src/app/component/dashboard/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerInfo } from '../../../model/customer.info';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  customerInfo: CustomerInfo;
   constructor() { }
 
   ngOnInit() {
+  }
+  evaluate() {
+    console.log(this.customerInfo.fullName);
   }
 
 }
